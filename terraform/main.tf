fa306c28253096e9b95555ed2daf15888afcfad6
Deepak Saxena <saxenad592@gmail.com>
2026-02-02 16:15:06 +0530

    Add profile image to professional website

---
+++ terraform/main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+################################
+# VPC (NO NAT = cheaper)
+################################
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = false
+  single_nat_gateway = false
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  map_public_ip_on_launch = true
+}
+
+################################
+# EKS CLUSTER (FIXED)
+################################
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "professional-website-eks"
+  cluster_version = "1.29"
+
+  # 🔑 CRITICAL FIXES
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = false
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
+    }
+  }
+}
+
